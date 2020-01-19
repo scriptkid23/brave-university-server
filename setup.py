1965c90a3780c3bf23f68cb1cb9ad23b19bd31a4
ICON <sieunhannhenkutesn@gmail.com>
2020-01-20 00:34:34 +0700

    Update setup.py 😅

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ description='Brave University of Package',
 url='#',
 author='HoanDo',
 license='MIT',
-packages=['constants','config','api','controllers','models'],
+packages=['constants','config','api','controllers','models','utils'],
 zip_safe=False)
